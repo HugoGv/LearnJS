6aab506f8cdbc22b865fa3c175ca1bb1f8d654a6
Alexander Glazunov <aglazunow@servicechannel.com>
2020-02-17 13:17:23 +0300

    task2 --- code review v1.1

--- task2/js/app.js
+++ task2/js/app.js
@@ -4,28 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     for (const index in zombies) {
         const zombie = zombies[index];
 
-        switch (true) {
-            case zombie.type === ZOMBIE_TYPE.SMALL:
+        switch (zombie.type) {
+            case ZOMBIE_TYPE.SMALL:
                 zombie.icons = 'zombie-small';
                 break;
-            case zombie.type === ZOMBIE_TYPE.MAD:
+            case ZOMBIE_TYPE.MAD:
                 zombie.icons = 'zombie-mad';
                 break;
-            case zombie.type === ZOMBIE_TYPE.STRONG:
+            case ZOMBIE_TYPE.STRONG:
                 zombie.icons = 'zombie-strong';
         }
 
-        const div = document.createElement('div');
-        div.classList.add('zombie-item');
+        const zombieElement = document.createElement('div');
+        zombieElement.classList.add('zombie-item', zombie.icons);
 
-        const img = document.createElement('img');
-        img.setAttribute('src', `icons/${zombie.icons}.gif`);
-        img.setAttribute('alt', zombie.icons);
+        zombieElement.addEventListener('click', (event) => {
+            event.target.classList.add('zombie-dead');
+        });
 
-        wrapper.appendChild(div).appendChild(img);
+        wrapper.appendChild(zombieElement);
     }
-
-    wrapper.addEventListener('click', (event) => {
-        event.target.src = 'icons/zombie-dead.gif';
-    });
 });
